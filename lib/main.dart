0ea734dce33a1e24aed100458b701b74b5f17e56
vostreex <vostan97@gmail.com>
2025-11-11 15:11:10 +0300

    DI changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'app.dart';
-import 'package:prack_9/data/note_store.dart';
+import 'features/notes/models/note.dart';
+import 'features/notes/state/add_note_store.dart';
+import 'features/notes/state/archive_store.dart';
+import 'features/notes/state/edit_note_store.dart';
+import 'features/notes/state/favorites_store.dart';
+import 'features/notes/state/notes_list_store.dart';
+import 'package:mobx/mobx.dart';
 
 void main() {
-  GetIt.I.registerSingleton<NoteStore>(NoteStore());
+  GetIt.I.registerSingleton<ObservableList<Note>>(ObservableList<Note>());
+  GetIt.I.registerSingleton<AddNoteStore>(AddNoteStore());
+  GetIt.I.registerSingleton<EditNoteStore>(EditNoteStore());
+  GetIt.I.registerSingleton<NotesListStore>(NotesListStore());
+  GetIt.I.registerSingleton<ArchiveStore>(ArchiveStore());
+  GetIt.I.registerSingleton<FavoritesStore>(FavoritesStore());
   runApp(MyApp());
 }
